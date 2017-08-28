47f5a17634e5b33d59403dcf12389636c1f66595
Jason Kridner <jdk@ti.com>
2017-08-28 18:00:01 +0000

    Open framebuffer

--- opencv_demo/opencv_demo.cpp
+++ opencv_demo/opencv_demo.cpp
@@ -4,6 +4,15 @@
 #include <string>
 #include <chrono>
 #include <thread>
+#ifndef WRITE_JPEG
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/ioctl.h>
+#include <sys/mman.h>
+#include <linux/fb.h>
+#endif
 #include "lepton3_grabber.hpp"
 
 #include <opencv2/core/core.hpp>
@@ -11,14 +20,21 @@
 
 using namespace std;
 
-static bool close = false;
+static bool do_exit = false;
+#ifndef WRITE_JPEG
+int fbfd = 0;
+struct fb_fix_screeninfo finfo;
+long int screensize = 0;
+char *fbp = 0;
+#endif
 
-void close_handler(int s)
+
+void exit_handler(int s)
 {
 	if(s==2)
 	{
 		cout << endl << "Ctrl+C pressed..." << endl;		
-		close = true;
+		do_exit = true;
 	} 
 }
 
@@ -29,7 +45,7 @@ int main (int argc, char *argv[])
 	// >>>>> Enable Ctrl+C
 	struct sigaction sigIntHandler;
 
-    sigIntHandler.sa_handler = close_handler;
+    sigIntHandler.sa_handler = exit_handler;
     sigemptyset(&sigIntHandler.sa_mask);
     sigIntHandler.sa_flags = 0;
 
@@ -43,15 +59,38 @@ int main (int argc, char *argv[])
 	uint64_t frameIdx=0;
 	char image_name[32];
 	
-	while(!close)
+#ifndef WRITE_JPEG		
+	fbfd = open("/dev/fb0", O_RDWR);
+
+	if (ioctl(fbfd, FBIOGET_FSCREENINFO, &finfo)) {
+		printf("Error reading screen information.\n");
+		exit(-1);
+	}
+
+	// map fb to user mem 
+	screensize = finfo.smem_len;
+	fbp = (char*)mmap(0, 
+		screensize, 
+		PROT_READ | PROT_WRITE, 
+		MAP_SHARED, 
+		fbfd, 
+		0);
+
+	if ((int)fbp == -1) {
+		printf("Failed to mmap.\n");
+	}
+#endif
+
+	while(!do_exit)
 	{
 		int w;
 		int h;
 		
 		const char* data = grabber.getLastFrame( &w, &h );
-		
+
 		if( data && w!=-1 && h!=-1 )
 		{
+#ifdef WRITE_JPEG		
 			sprintf(image_name, "IMG_%.6d.jpg", frameIdx);
 			string imgStr = image_name;
 			
@@ -63,12 +102,21 @@ int main (int argc, char *argv[])
 			cout << "Saved: " << image_name << "[" << w << "x" << h << "]" << endl;
 			
 			frameIdx++;
+#else
+			cv::Mat frame( h,w, CV_8UC1 );
+			memcpy( frame.data, data, w*h );
+#endif
 		}
 		
 		std::this_thread::sleep_for(std::chrono::milliseconds(100));
 	}
 	
 	grabber.stop();
+
+#ifndef WRITE_JPEG
+	munmap(fbp, screensize);
+	close(fbfd);
+#endif
 
     return EXIT_SUCCESS;
 }
